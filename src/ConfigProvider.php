6d6ccaeb8f4e96ba418171e0577ebd6dc572e7a5
Angel Martz <angel.martz24@gmail.com>
2021-01-28 20:48:54 -0600

    Added Abstract Factories

---
+++ src/ConfigProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AngelMartz\DoctrineODMMezzio;
+
+use AngelMartz\DoctrineODMMezzio\Service;
+
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'doctrine' => $this->getDoctrineConfig(),
+            'doctrine_factories' => $this->getDoctrineFactoryConfig()
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     */
+    public function getDependencies(): array
+    {
+        return [
+            'abstract_factories' => [
+                Service\ServiceAbstractFactory::class,
+            ],
+        ];
+    }
+
+    public function getDoctrineConfig() : array
+    {
+        return [];
+    }
+
+    public function getDoctrineFactoryConfig(): array
+    {
+        return [];
+    }
+    
+}
